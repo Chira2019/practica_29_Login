727e482b41261357d614a2c110403cafa567e6fb
Chira <betochirakudai@gmail.com>
2021-07-06 12:40:59 -0500

    y

---
+++ js/index.js
@@ -0,0 +1,10 @@
+let limpiar = document.getElementById("Limpiar");
+
+limpiar = function(){
+    nombre = document.getElementById("nombre").value="";
+    apelldio = document.getElementById("apellido").value="";
+    edad = document.getElementById("edad").value="";
+    nota1 = document.getElementById("nota_1").value="";
+    nota_2 = document.getElementById("nota_2").value="";
+}
+
